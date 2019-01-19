f836c7f9f1e9cb08fa7126573d792408c907183c
Pavel Oleynik <uns0ulg@gmail.com>
2019-01-20 00:01:35 +0200

    Add two function

--- hw3/Task1.js
+++ hw3/Task1.js
@@ -3,6 +3,7 @@
  * @param {Array<number>} input
  * @returns {Array<number>} Returns array that contains positive numbers only.
  */
+
 function negativeFilter(input) {
     let arrPositive = [];
     input.forEach(function (item) {
@@ -11,6 +12,14 @@ function negativeFilter(input) {
     return console.log(arrPositive);
 }
 
+function isPositive(input) {
+    input.forEach(function (item) {
+        (item > 0) ? console.log(item + ' is a positive number. TRUE') : console.log(item + ' is a negative number. FALSE');
+    });
+}
+
 module.exports = negativeFilter;
 
-negativeFilter([1, -2 ,3 ,4 ,5 ,-6 ,-7 ,8]);
+arr = [1, -2 ,3 ,4 ,5 ,-6 ,-7 ,8];
+negativeFilter(arr);
+isPositive(arr);
