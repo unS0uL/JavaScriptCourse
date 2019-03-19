742bfc61c637c34d8cf8e960a4a60f53a482f0a4
Oleynik Pavel <uns0ulg@gmail.com>
2019-03-19 14:40:38 +0200

    add

---
+++ hw4/Task1.js
@@ -0,0 +1,22 @@
+/**
+ * Calculates Сommon Divisor
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number} Returns Сommon Divisor.
+ */
+
+const commonDevisor = (num1, num2) => {
+    minNum = 0;
+    devisor = 0;
+    num1 < num2 ? minNum = num1 : minNum = num2;
+    for (let i = 0; i < minNum; i++){
+        if (num1 % i === 0 && num2 % i === 0) {
+            devisor = i;
+        }
+    }
+    return devisor;
+};
+
+module.exports = commonDevisor;
+
+console.log(commonDevisor(4, 10));
