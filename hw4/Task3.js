08ae8f12abfa0ee9c4216af3fe0f0ace582125a7
Oleynik Pavel <uns0ulg@gmail.com>
2019-03-22 01:28:10 +0200

    add

--- hw4/Task3.js
+++ hw4/Task3.js
@@ -5,14 +5,13 @@
  */
 const advancedFilter = (input) => {
     return input.filter(function (digit, i) {
-        if (((digit % 3) && (digit % 5) === 0) || (digit % 7 === 0)) {
+        if (((digit % 3 === 0) && (digit % 5 === 0)) || (digit % 7 === 0)) {
             return input[i];
         }
     });
 };
 
 module.exports = advancedFilter;
-
 
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
