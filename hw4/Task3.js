f8df50c4872e56d8cd1df5a6d3aa4ed3406fff7a
Oleynik Pavel <uns0ulg@gmail.com>
2019-03-22 01:21:06 +0200

    add

--- hw4/Task3.js
+++ hw4/Task3.js
@@ -4,7 +4,17 @@
  * @returns {Array<number>} Returns filtered array.
  */
 const advancedFilter = (input) => {
-    return;
+    let arr2 = input.filter(function(digit, i) {
+        if (((digit % 3) && (digit % 5) === 0) || (digit % 7 === 0)) {
+            return input[i];
+        }
+    });
+    return arr2;
 };
 
 module.exports = advancedFilter;
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+console.log(advancedFilter(arr));
