332bcdc99fb54564528418958bd0de1526831355
Oleynik Pavel <uns0ulg@gmail.com>
2019-04-12 12:48:57 +0300

    add

--- hw4/Task4.js
+++ hw4/Task4.js
@@ -6,25 +6,23 @@
  */
 const maxMin = (input, order = true) => {
     let lowHighVal = 1;
-        input.forEach(function(item) {
-            if (order) {
-                // lowHighVal = 0;
-                if (lowHighVal < item ) {
-                    lowHighVal = item;
-                }
-            } else {
-                // lowHighVal = 1;
-                if (item < lowHighVal) {
-                    lowHighVal = item;
-                }
+    input.forEach(function (item) {
+        if (order) {
+            if (lowHighVal < item) {
+                lowHighVal = item;
             }
-        });
-     return lowHighVal;
+        } else {
+            if (item < lowHighVal) {
+                lowHighVal = item;
+            }
+        }
+    });
+    return lowHighVal;
 };
 
 module.exports = maxMin;
 
-let arr = [12,13,14,15,16,17,18,1,2,3,4,5,6,7,8,9,10,11];
+let arr = [12, 13, 14, 15, 16, 17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 console.log(maxMin(arr, true));
 console.log(maxMin(arr, false));
