c41d460d979a87e6001650dbe907380055f8a1f7
Oleynik Pavel <uns0ulg@gmail.com>
2019-04-12 15:05:19 +0300

    Task 1 is BEGIN!

---
+++ hw5/Task1.js
@@ -0,0 +1,23 @@
+
+/**
+ * String encription
+ * @param {string} text
+ * @param {number} n
+ * @returns {string} Returns Encrypted string.
+ */
+
+const encrypt = (text, n) => {
+    return
+};
+
+/**
+ * String encription
+ * @param {string} text
+ * @param {number} n
+ * @returns {string} Returns Decrypted string.
+ */
+const decrypt = (encryptedText, n) => {
+
+}
+
+module.exports = { encrypt , decrypt };
