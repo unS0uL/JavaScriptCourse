071087285718fb41dca1096a84dc8c7c0fef4886
Oleynik Pavel <uns0ulg@gmail.com>
2019-04-12 13:54:25 +0300

    add

--- hw4/Task5.js
+++ hw4/Task5.js
@@ -4,8 +4,23 @@
  * @param {Array<number>} input
  * @returns {Array<number>} Returns filtered array.
  */
+
 const moveZeros = function (arr) {
-    return
+    let colectZerro = [];
+    let changedArr = arr.filter(function(item, i) {
+        if (!item) {
+            colectZerro.push(item);
+        }
+            return item;
+    });
+    colectZerro = colectZerro.join(',');
+    changedArr = changedArr.join(',') + ',' + colectZerro;
+   return changedArr.split(',');
 }
 
 module.exports = moveZeros;
+
+let arr = [12, 13, 0, 15, 16, 17, 0, 1, 2, 3, 0, 5, 6, 7, 8, 0, 10, 11];
+
+
+console.log(moveZeros(arr));
