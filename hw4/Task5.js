907c6fc4c4469084a2317b888dd3c522dce859d5
Oleynik Pavel <uns0ulg@gmail.com>
2019-04-12 14:15:52 +0300

    add

--- hw4/Task5.js
+++ hw4/Task5.js
@@ -1,26 +1,20 @@
-
 /**
  * Filter array. Move zeros in the end of string.
- * @param {Array<number>} input
  * @returns {Array<number>} Returns filtered array.
+ * @param arr
  */
 
 const moveZeros = function (arr) {
-    let colectZerro = [];
-    let changedArr = arr.filter(function(item, i) {
+    arr.filter(function (item, i) {
         if (!item) {
-            colectZerro.push(item);
+            arr.splice(i, 1);
+            arr.push(item);
         }
-            return item;
     });
-    colectZerro = colectZerro.join(',');
-    changedArr = changedArr.join(',') + ',' + colectZerro;
-   return changedArr.split(',');
-}
+    return arr;
+};
 
 module.exports = moveZeros;
 
 let arr = [12, 13, 0, 15, 16, 17, 0, 1, 2, 3, 0, 5, 6, 7, 8, 0, 10, 11];
-
-
 console.log(moveZeros(arr));
